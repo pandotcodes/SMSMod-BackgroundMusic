dda21d92978295c1057491c857af38f655893a44
Jonathan Riedel <pn@bptv.eu>
2024-05-02 19:07:58 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BackgroundMusic
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
